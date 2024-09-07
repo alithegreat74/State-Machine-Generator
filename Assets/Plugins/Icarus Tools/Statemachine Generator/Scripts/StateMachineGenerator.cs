a3c0cf973826dc4f4ce66c67db688404c894f23e
Alireza Mohammadi <86660759+alithegreat74@users.noreply.github.com>
2024-09-07 16:48:05 +0330

    added player scripts generation
    - changed templates
    - chagened statemachinegenerator class

--- Assets/Plugins/Icarus Tools/Statemachine Generator/Scripts/StateMachineGenerator.cs
+++ Assets/Plugins/Icarus Tools/Statemachine Generator/Scripts/StateMachineGenerator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,13 +11,15 @@ using UnityEngine;
 #if UNITY_EDITOR
 public class StateMachineGenerator : EditorWindow
 {
-    private List<string> _playerStates = new List<string>();
+    private List<string> _states = new List<string>();
     private Vector2 _scrollPos;
     private int _selectedTab = 0;
-    private string _baseClassPath;
-    private string _enemyClassPath;
-    private string _playerClassPath;
-    private const string _baseClassTemplateFolder = "Assets/Icarus Tools/Statemachine Generator/Script Templates/Base Classes";
+    private string _path;
+    private const string _baseClassTemplateFolder = "Assets/Plugins/Icarus Tools/Statemachine Generator/Script Templates/Base Classes";
+    #region templateNames
+    private readonly string[] _baseTemplates = new string[] { $"{_baseClassTemplateFolder}/Entity.txt", $"{_baseClassTemplateFolder}/State.txt", $"{_baseClassTemplateFolder}/Statemachine.txt" };
+    private readonly string[] _baseEnemyClass = new string[] { $"{_baseClassTemplateFolder}/Enemy.txt", $"{_baseClassTemplateFolder}/EnemyState.txt" };
+    #endregion
 
 
     [MenuItem("Tools/IcarusTools/State Machine Generator")]
@@ -41,6 +44,7 @@ public class StateMachineGenerator : EditorWindow
                 CreateEnemyClasses_UI();
                 break;
         }
+
     }
 
     private void CreateBaseClass_UI()
@@ -49,45 +53,64 @@ public class StateMachineGenerator : EditorWindow
         GUILayout.Label("By Clicking The Create Structure button, the necessary classes like Entity\n, and State And Statemachine will be added to your project");
 
         GUILayout.Label("Enter the folder you want the scripts be generated (Absolute Path)");
-        _baseClassPath = EditorGUILayout.TextField("", _baseClassPath);
+        _path = EditorGUILayout.TextField("", _path);
 
         if (GUILayout.Button("Create Base Class Structure"))
         {
-            ScriptGenerator.GenerateScript($"{_baseClassTemplateFolder}/Entity.txt",$"{_baseClassPath}/Entity.cs");
-            ScriptGenerator.GenerateScript($"{_baseClassTemplateFolder}/State.txt",$"{_baseClassPath}/State.cs");
-            ScriptGenerator.GenerateScript($"{_baseClassTemplateFolder}/Statemachine.txt", $"{_baseClassPath}/Statemachine.cs");
+            GenerateScripts(_baseTemplates,new Dictionary<string, string>());
         }
     }
+
+
     private void CreatePlayerClasses_UI()
     {
         GUILayout.Label("By Clicking The Create Player Classes button, the necessary classes like\nPlayer, PlayerState and all the states defined by you here will be added to your project");
         GUILayout.Label("Enter the folder you want the scripts be generated (Absolute Path)");
-        _playerClassPath = EditorGUILayout.TextField("", _playerClassPath);
+        _path = EditorGUILayout.TextField("", _path);
 
-        if (_playerStates.Count <= 0)
-            _playerStates.Add("");
+        if (_states.Count <= 0)
+            _states.Add("");
         GUILayout.Label("Enter Your State Names");
 
         _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos);
 
-        for (int i = 0; i < _playerStates.Count; i++)
+        for (int i = 0; i < _states.Count; i++)
         {
             EditorGUILayout.BeginHorizontal();
 
-            _playerStates[i] = EditorGUILayout.TextField("State Name", _playerStates[i]);
+            _states[i] = EditorGUILayout.TextField("State Name", _states[i]);
 
             if (GUILayout.Button("Remove", GUILayout.Width(60)))
             {
-                _playerStates.RemoveAt(i);
+                _states.RemoveAt(i);
             }
 
             EditorGUILayout.EndHorizontal();
         }
         if (GUILayout.Button("Add States", GUILayout.Width(100)))
         {
-            _playerStates.Add("");
+            _states.Add("");
         }
         EditorGUILayout.EndScrollView();
+
+        if (GUILayout.Button("Create Scripts"))
+        {
+            GenerateScript($"{_baseClassTemplateFolder}/PlayerState.txt", "PlayerState.cs", new Dictionary<string, string>());
+
+            string stateInitializatoin = "";
+            string stateDeclaration = "";
+            foreach(var state in _states)
+            {
+                stateDeclaration += $"public Player{state}State {state}State {{ get; private set;}}\n\t";
+                stateInitializatoin += $"{state}State = new Player{state}State(this,statemachine,\"{state}\",this);\n\t\t";
+                var dictionary = new Dictionary<string, string>() { { "#StateName", state } };
+                GenerateScript($"{_baseClassTemplateFolder}/PlayerTemplateState.txt", $"Player{state}State.cs", dictionary);
+            }
+
+            var dict = new Dictionary<string, string>() { { "#StateDeclaration", stateDeclaration }, { "#StatesInitialization", stateInitializatoin }, { "#StatemachineInitialization", $"statemachine.Initialize({_states[0]}State);" } };
+            GenerateScript($"{_baseClassTemplateFolder}/Player.txt", "Player.cs", dict);
+
+        }
     }
 
     private void CreateEnemyClasses_UI()
@@ -95,13 +118,31 @@ public class StateMachineGenerator : EditorWindow
         GUILayout.Label("By Clicking The Create Structure button, the necessary classes like Enemy\n, and EnemyState will be added to your project");
 
         GUILayout.Label("Enter the folder you want the scripts be generated (Absolute Path)");
-        _enemyClassPath = EditorGUILayout.TextField("", _enemyClassPath);
+        _path = EditorGUILayout.TextField("", _path);
 
         if (GUILayout.Button("Create Base Class Structure"))
         {
-            ScriptGenerator.GenerateScript($"{_baseClassTemplateFolder}/Enemy.txt", $"{_enemyClassPath}/Enemy.cs");
-            ScriptGenerator.GenerateScript($"{_baseClassTemplateFolder}/EnemyState.txt", $"{_enemyClassPath}/EnemyState.cs");
+            GenerateScripts(_baseEnemyClass, new Dictionary<string, string>());   
         }
+    }
+
+    private void GenerateScripts(string[] templates, Dictionary<string, string> replacements)
+    {
+        foreach (var template in templates)
+        {
+            var filename=new FileInfo(template);
+            string fname = filename.Name.Replace(".txt", ".cs");
+            GenerateScript(template, fname, replacements);
+        }
+    }
+
+    private void GenerateScript(string template, string scriptName, Dictionary<string, string> replacements)
+    {
+        
+        var temp = new ScriptTemplate(template);
+        FileManager.GenerateScript(temp.ReplaceWords(replacements), $"{_path}/{scriptName}");
+
+        AssetDatabase.Refresh();
     }
 }
 
